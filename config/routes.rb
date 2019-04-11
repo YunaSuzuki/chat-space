b58dbbec0688ccd8b97d383c8c12f5ff51ff20c6
Yuna Suzuki <yuna.com.jp@outlook.jp>
2019-04-11 22:23:22 +0900

    to-master

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root    'messages#index'
-  get     'groups/new'      => 'groups#new'
-  get     'users/edit'      => 'users#edit'
-  get     'groups/edit'     => 'groups#edit'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
